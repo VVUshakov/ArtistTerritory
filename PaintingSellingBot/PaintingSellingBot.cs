3f1a40c56d673d4fb3aaa94eff1391fede2e1793
Владимир Ушаков <svayznoy@inbox.ru>
2024-03-17 21:13:31 +0700

    Убрать using'и и выполнить явное приведение типов

--- PaintingSellingBot/PaintingSellingBot.cs
+++ PaintingSellingBot/PaintingSellingBot.cs
@@ -1,25 +1,119 @@
-﻿using PaintingSellingBot.Configs;
-using PRTelegramBot.Configs;
-using PRTelegramBot.Core;
-
-
-#region [Запуск PaintingSellingBot]
+﻿#region [Запуск Бота]
 
 // Получить конфигурацию бота
-var telegramConfig = ConfigApp.GetSettingsBot<TelegramConfig>();
+PRTelegramBot.Configs.TelegramConfig telegramConfig = PaintingSellingBot.Configs.ConfigApp.GetSettingsBot<PRTelegramBot.Configs.TelegramConfig>();
 // Получить экземпляр бота из библиотеки PRTelegramBot
-var paintingSellingBot = new PRBot(telegramConfig);
+PRTelegramBot.Core.PRBot bot = new(telegramConfig);
 
 // Подписаться на общие логи
-paintingSellingBot.OnLogCommon += Telegram_OnLogCommon;
+bot.OnLogCommon += Telegram_OnLogCommon;
 // Подписаться на логи ошибок
-paintingSellingBot.OnLogError += Telegram_OnLogError;
+bot.OnLogError += Telegram_OnLogError;
 
 // Запустить бот
-await paintingSellingBot.Start();
+await bot.Start();
 
-#endregion [Запуск PaintingSellingBot]
+// Инициализировать обработчики событий
+HandlerInit(bot);
 
+#endregion
+
+// Подписать на события
+void HandlerInit(PRTelegramBot.Core.PRBot tg)
+{
+	if(tg.Handler != null)
+	{
+		//Обработка обновление 
+		tg.Handler.OnPreUpdate += Handler_OnUpdate;
+
+		//Обработка обновление кроме message и callback
+		tg.Handler.OnPostMessageUpdate += Handler_OnWithoutMessageUpdate;
+
+		//Обработка не правильный тип сообщений
+		tg.Handler.Router.OnWrongTypeMessage += ExampleEvent.OnWrongTypeMessage;
+
+		//Обработка пользователь написал в чат start с deeplink
+		tg.Handler.Router.OnUserStartWithArgs += ExampleEvent.OnUserStartWithArgs;
+
+		//Обработка проверка привилегий
+		tg.Handler.Router.OnCheckPrivilege += ExampleEvent.OnCheckPrivilege;
+
+		//Обработка пропущенной  команды
+		tg.Handler.Router.OnMissingCommand += ExampleEvent.OnMissingCommand;
+
+		//Обработка не верного типа чата
+		tg.Handler.Router.OnWrongTypeChat += ExampleEvent.OnWrongTypeChat;
+
+		//Обработка локаций
+		tg.Handler.Router.OnLocationHandle += ExampleEvent.OnLocationHandle;
+
+		//Обработка контактных данных
+		tg.Handler.Router.OnContactHandle += ExampleEvent.OnContactHandle;
+
+		//Обработка голосований
+		tg.Handler.Router.OnPollHandle += ExampleEvent.OnPollHandle;
+
+		//Обработка WebApps
+		tg.Handler.Router.OnWebAppsHandle += ExampleEvent.OnWebAppsHandle;
+
+		//Обработка, когда пользователю отказано в доступе
+		tg.Handler.Router.OnAccessDenied += ExampleEvent.OnAccessDenied;
+
+		//Обработка сообщения с документом
+		tg.Handler.Router.OnDocumentHandle += ExampleEvent.OnDocumentHandle;
+
+		//Обработка сообщения с аудио
+		tg.Handler.Router.OnAudioHandle += ExampleEvent.OnAudioHandle;
+
+		//Обработка сообщения с видео
+		tg.Handler.Router.OnVideoHandle += ExampleEvent.OnVideoHandle;
+
+		//Обработка сообщения с фото
+		tg.Handler.Router.OnPhotoHandle += ExampleEvent.OnPhotoHandle;
+
+		//Обработка сообщения с стикером
+		tg.Handler.Router.OnStickerHandle += ExampleEvent.OnStickerHandle;
+
+		//Обработка сообщения с голосовым сообщением
+		tg.Handler.Router.OnVoiceHandle += ExampleEvent.OnVoiceHandle;
+
+		//Обработка сообщения с неизвестным типом
+		tg.Handler.Router.OnUnknownHandle += ExampleEvent.OnUnknownHandle;
+
+		//Обработка сообщения с местоположением
+		tg.Handler.Router.OnVenueHandle += ExampleEvent.OnVenueHandle;
+
+		//Обработка сообщения с игрой
+		tg.Handler.Router.OnGameHandle += ExampleEvent.OnGameHandle;
+
+		//Обработка сообщения с видеозаметкой
+		tg.Handler.Router.OnVideoNoteHandle += ExampleEvent.OnVideoNoteHandle;
+
+		//Обработка сообщения с игральной костью
+		tg.Handler.Router.OnDiceHandle += ExampleEvent.OnDiceHandle;
+
+	}
+
+	tg.RegisterInlineCommand(AddCustomTHeader.TestAddCommand, async (botClient, update) =>
+	{
+		PRTelegramBot.Helpers.Message.Send(botClient, update, "Тест метода TestAddCommand");
+	});
+
+	tg.RegisterInlineCommand(AddCustomTHeader.TestAddCommandTwo, async (botClient, update) =>
+	{
+		PRTelegramBot.Helpers.Message.Send(botClient, update, "Тест метода TestAddCommandTwo");
+	});
+}
+
+// Обработчик_Обновление 
+async Task<PRTelegramBot.Models.Enums.ResultUpdate> Handler_OnUpdate(Telegram.Bot.ITelegramBotClient client, Telegram.Bot.Types.Update update)
+	=> PRTelegramBot.Models.Enums.ResultUpdate.Continue;
+
+// Обрабочик_Обновление (кроме message и callback)
+async Task Handler_OnWithoutMessageUpdate(Telegram.Bot.ITelegramBotClient botclient, Telegram.Bot.Types.Update update)
+{
+	//Обработка обновление кроме message и callback
+}
 
 #region [Держать консоль открытой]
 
@@ -40,7 +134,7 @@ while(true)
 	}
 }
 
-#endregion [Держать консоль открытой]
+#endregion
 
 #region [Логгирование]
 
@@ -62,4 +156,4 @@ void Telegram_OnLogCommon(string msg, Enum? eventType, ConsoleColor color = Cons
 	Console.ResetColor();
 }
 
-#endregion [Логгирование]
+#endregion
