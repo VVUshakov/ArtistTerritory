b41bf8ab2c7530e1d350022883c4d3aaf4fe4dc3
Владимир Ушаков <svayznoy@inbox.ru>
2024-03-31 22:57:34 +0700

    Разделить проект на множество микросервисов

---
+++ Service.ConnectingDatabase/Data/GalleryDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PaintingSellingBot.Extension;
+
+
+namespace Service.ConnectingDatabase.Data
+{
+    /// <summary>
+    /// Класс для контекста базы данных.
+    /// Класс GalleryContext наследуется от DbContext и определяет набор сущностей, 
+    /// которые будут управляться этим контекстом.
+    /// </summary>
+    public class GalleryDbContext : DbContext
+    {
+        //TODO: Изменить на получение из конфигурационного файла
+        private static readonly string nameDb = "SQLite";
+
+        //TODO: Изменить на получение из конфигурационного файла
+        // TODO: Нужно заменить "YourConnectionStringHere" на реальную строку подключения
+        private static readonly string connectionStringDb = "YourConnectionStringHere";
+
+        public GalleryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Picture> Pictures { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Technique> Techniques { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        /// <summary>
+        /// Используется для настройки подключения к базе данных
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Вызов метода расширения для выбора базы данных
+            optionsBuilder.UseDatabase(nameDb);
+        }
+
+        /// <summary>
+        /// Используется для настройки моделей сущностей и связей между ними (конфигурация связей)
+        /// </summary>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // "Картина" имеет связь с "Художник" через свойство ArtistID
+            modelBuilder.Entity<Picture>()
+                .HasOne(p => p.Artist)
+                .WithMany()
+                .HasForeignKey(p => p.ArtistID);
+
+            // "Картина" имеет связь с "Техника исполнения" через свойство TechniqueID
+            modelBuilder.Entity<Picture>()
+            .HasOne(p => p.Technique)
+            .WithMany()
+            .HasForeignKey(p => p.TechniqueID);
+
+            // "Картина" имеет связь с "Жанр" через свойство GenreID
+            modelBuilder.Entity<Picture>()
+                .HasOne(p => p.Genre)
+                .WithMany()
+                .HasForeignKey(p => p.GenreID);
+        }
+    }
+}
